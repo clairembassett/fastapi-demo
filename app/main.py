9727f494d4af7dc55e65df59d2f1cc1073870c0c
clairembassett <clairemckbassett@gmail.com>
2024-11-18 14:45:25 -0500

    changes

--- app/main.py
+++ app/main.py
@@ -1,22 +1,70 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import pandas as pd
 import json
 import os
+import mysql.connector
+from mysql.connector import Error
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")  # zone apex
-def zone_apex():
-    return {"Hello": "Claire!"}
 
-@app.get("/add/{a}/{b}")
-def add(a: int, b: int):
-    return {"sum": a + b}
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "ds2022"
+DBPASS = os.getenv('DBPASS')
+DB = "qxm6fm"
 
-@app.get("/multiply/{c}/{d}")
-def multiply(c:int, d: int):
-	return {product: {c}/{d}}
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur=db.cursor()
 
+@app.get('/genres')
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
+
+@app.get('/songs')
+def get_songs(): 
+    query = """
+	SELECT
+		songs.title AS title,
+		albums.name AS album,
+		artists.name AS artist,
+		songs.year AS year,
+		CONCAT('https://s3.amazonaws.com/yourbucket/', songs.file) AS file,
+		CONCAT('https://s3.amazonaws.com/yourbucket/', songs.image) AS image,
+		genres.name AS genre
+	FROM songs
+	JOIN albums on songs.albumid = albums.albumid
+	JOIN artists ON albums.artistid = artists.artistid
+	JOIN genres ON songs.genre = genres.genreid
+	ORDER BY songs.title;
+"""
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
+    except Error as e:
+        return {"Error": "MySQL Error: " + str(e)}
--- preview.sh
+++ preview.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd app
-uvicorn main:app --reload --log-level debug
+uvicorn main:app --reload --log-level debug --port 8888
